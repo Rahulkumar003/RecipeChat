c12fa4beaf406fde2acc12d69f15f96c723206ce
Param Kumar Raman <108082874+param934@users.noreply.github.com>
2025-05-13 16:57:33 +0530

    switched to GCP

--- src/components/NewChatView.js
+++ src/components/NewChatView.js
@@ -5,8 +5,8 @@ import { MdSend } from 'react-icons/md';
 import { io } from 'socket.io-client';
 
 //socket connection :
-const socket = io('https://backend-released-recipechat.onrender.com', {
-// const socket = io('https://recipe-chat-backend-532248491422.us-central1.run.app', {
+// const socket = io('https://backend-released-recipechat.onrender.com', {
+const socket = io('https://recipe-chat-backend-532248491422.us-central1.run.app', {
 
   transports: ['websocket', 'polling'],
 });
